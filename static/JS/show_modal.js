b5c2bc8aeb83bf479410a977053349d8f2a4ed6e
Aman <mamanullahusmani@gmail.com>
2019-04-05 01:27:11 -0400

    intial integration of feature layer to map layer

--- static/JS/show_modal.js
+++ static/JS/show_modal.js
@@ -4,6 +4,7 @@ var btn = document.getElementById("share-route-Btn");
 btn.onclick = function () {
     if (confirm('Are you sure you want to share your route with other people for carpooling?')) {
         // Save it!
+        gatherDataToAddInFeatureLayer();
     } else {
         // Do nothing!
     }
@@ -21,4 +22,18 @@ function tab(evt, tabName) {
   }
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " is-active";
+}
+
+function gatherDataToAddInFeatureLayer() {
+
+    var yearSelection = document.getElementById('vehicleYear');
+    var year = yearSelection.options[yearSelection.selectedIndex].value;
+
+    var makeSelection = document.getElementById('vehicleMake');
+    var make = makeSelection.options[makeSelection.selectedIndex].value;
+
+    var modelSelection = document.getElementById('vehicleModel');
+    var model = modelSelection.options[modelSelection.selectedIndex].value;
+
+    addFeature(point, year, make, model)
 }
