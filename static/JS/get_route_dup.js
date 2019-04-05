b15994ea766c3cb0e7fd910526a091cb688a7f91
Aman <mamanullahusmani@gmail.com>
2019-04-05 11:09:14 -0400

    added feature layer query initial functionality

--- static/JS/get_route_dup.js
+++ static/JS/get_route_dup.js
@@ -15,9 +15,11 @@ require([
     "esri/widgets/Sketch/SketchViewModel",
     "esri/layers/GraphicsLayer",
     "esri/layers/FeatureLayer",
+    "esri/geometry/Circle",
+    "esri/geometry/geometryEngine",
     "dojo/domReady!"
 ], function (Map, MapView, Graphic, RouteTask, RouteParameters, FeatureSet, Home, Locate, Search, BasemapToggle,
-             SketchViewModel, GraphicsLayer, FeatureLayer) {
+             SketchViewModel, GraphicsLayer, FeatureLayer, Circle, geometryEngine) {
     loadCSV();
 
     // GraphicsLayer to hold graphics created via sketch view model
@@ -36,7 +38,13 @@ require([
         zoom: 12
     });
 
-    intializeFeatureLayer(map);
+    var featureLayer = new FeatureLayer({
+            url: "https://services.arcgis.com/4TKcmj8FHh5Vtobt/arcgis/rest/services/Carpool_DataSheet/FeatureServer/0",
+            outFields: ["*"],
+            visible: true
+        });
+
+    map.add(featureLayer);
 
     var homeWidget = new Home({
         view: view
@@ -112,7 +120,8 @@ require([
         // add the graphic to the graphics layer
         sketchVHomeM.create("point");
         document.getElementById("homeButton").disabled = true;
-        view.on("click", function (event) {
+        var handler = view.on("click", function (event) {
+            handler.remove();
             addGraphic("home", event.mapPoint);
             console.log("Home");
             /*if (view.graphics.length === 0) {
@@ -136,7 +145,8 @@ require([
     drawDestButton.onclick = function () {
         sketchVDestM.create("point");
         document.getElementById("destButton").disabled = true;
-        view.on("click", function (event) {
+        var handler = view.on("click", function (event) {
+            handler.remove();
             addGraphic("destination", event.mapPoint);
             console.log("Destination");
             /*
@@ -161,19 +171,19 @@ require([
         document.getElementById("destButton").disabled = false;
         tempGraphicsLayer2.removeAll();
         view.graphics.removeAll();
-        document.getElementById("drive-time").innerHTML = 0;
-        document.getElementById("distance").innerHTML = 0;
+        var zeroValue = 0;
+        document.getElementById("drive-time").innerHTML = zeroValue;
+        document.getElementById("distance").innerHTML = zeroValue;
+        document.getElementById("co2-emission").innerHTML = zeroValue.toFixed(1);
+        document.getElementById("cost").innerHTML = zeroValue.toFixed(2);
     };
-
 
     // To allow access to the route service and prevent the user from signing in, do the Challenge step in the lab to set up a service proxy
     var routeTask = new RouteTask({
         url: "https://utility.arcgis.com/usrsvcs/appservices/TDWzWsvWrqyvJH5i/rest/services/World/Route/NAServer/Route_World/solve"
     });
 
-    var btn = document.getElementById("share-route-Btn");
-
-    btn.onclick = function () {
+    document.getElementById("share-route-Btn").onclick = function () {
         if (confirm('Are you sure you want to share your route with other people for carpooling?')) {
             // Save it!
             gatherDataToAddInFeatureLayer();
@@ -181,7 +191,15 @@ require([
             // Do nothing!
         }
     };
+    document.getElementById("find-carpool-btn").onclick = function () {
+        findCarPoolArroundMe();
+    };
 
+    //var distanceSlider = document.getElementById("distance");
+    //var distanceValue = document.getElementById("distance-value");
+    //distanceSlider.addEventListener("input", function() {
+    //      distanceValue.innerText = distanceSlider.value;
+    //});
 
     function addGraphic(name, point) {
         var graphic = new Graphic({
@@ -191,12 +209,10 @@ require([
             }
         });
         view.graphics.add(graphic);
-
         console.log(view.graphics.length);
         if(view.graphics.length === 2)
             getRoute();
     }
-
 
     function getRoute() {
         // Setup the route parameters
@@ -228,21 +244,8 @@ require([
         });
     }
 
-    function intializeFeatureLayer() {
-
-        featureLayer = new FeatureLayer({
-            url: "https://services.arcgis.com/4TKcmj8FHh5Vtobt/arcgis/rest/services/Carpool_DataSheet/FeatureServer/0",
-            outFields: ["*"],
-            visible: true
-        });
-
-        map.add(featureLayer);
-    }
-
     function addFeature(point, carYear, carMake, carModel, latitude, longitude, name, email) {
-
-        console.log(point.x, point.y, carYear, carMake, carModel, latitude, longitude, name, email);
-
+        //console.log(point.x, point.y, carYear, carMake, carModel, latitude, longitude, name, email);
         const attributes = {};
         attributes["Car_Year"] = carYear;
         attributes["Car_Make"] = carMake;
@@ -268,7 +271,6 @@ require([
     }
 
     function gatherDataToAddInFeatureLayer() {
-
         var yearSelection = document.getElementById('vehicleYear');
         var year = yearSelection.options[yearSelection.selectedIndex].value;
 
@@ -292,11 +294,168 @@ require([
                 longitude = graphic.geometry.y;
                 console.log("Found Home", latitude, longitude);
             }
-        })
+        });
 
         var name = document.getElementById(id = 'user_name');
         var email = document.getElementById(id = 'user_email');
 
         addFeature(point, year, make, model, latitude, longitude, name.value, email.value);
+    }
+
+    function queryFeatureLayer(graphicGeometryHome){
+        /*
+        // query all features from the featureLayer layer
+        view
+          .when(function() {
+            return featureLayer.when(function() {
+              var query = featureLayer.createQuery();
+              return featureLayer.queryFeatures(query);
+            });
+          })
+          .then(getValues)
+          .then(addToSelect)
+          .then(createBuffer);
+
+        // return an array of all the values in the
+        // STATUS2 field of the wells layer
+        function getValues(response) {
+          var features = response.features;
+          var values = features.map(function(feature) {
+            return feature.attributes.Latitude;
+          });
+          return values;
+        }*/
+
+        function queryForFeatureGeometries() {
+          var featureQuery = featureLayer.createQuery();
+
+          return featureLayer.queryFeatures(featureQuery).then(function(response) {
+            var featuresGeometry = response.features.map(function(feature) {
+                var mapPoint = new MapPoint(feature.attributes.Latitude, feature.attributes.Longitude);
+                return mapPoint;
+            });
+
+            return featuresGeometry;
+          });
+        }
+
+
+
+        /*var query = featureLayer.createQuery();
+        query.geometry = graphicGeometryHome;
+        query.spatialRelationship = "intersects";
+        return featureLayer.queryFeatures(query);*/
+    }
+
+    function findCarPoolArroundMe() {
+        var homeBufferDist = 1000;//parseInt(distanceSlider.value);
+        //console.log(homeBufferDist);
+        //var isMiles = true;
+
+        var graphicGeometryHome;
+        var graphicGeometryDest;
+        view.graphics.forEach(function (graphic) {
+            if(graphic.attributes["name"] == "destination") {
+                graphicGeometryDest = graphic.geometry;
+                //console.log("Found Destination");
+            }
+            else if (graphic.attributes["name"] == "home") {
+                graphicGeometryHome = graphic.geometry;
+                //console.log("Found Home");
+            }
+        })
+
+        /*var point = {
+          type: "point", // autocasts as new Point()
+          longitude: -49.97,
+          latitude: 41.73
+        };*/
+
+        /*var markerSymbol = {
+          type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
+          color: [226, 119, 40],
+          outline: {
+            // autocasts as new SimpleLineSymbol()
+            color: [255, 255, 255],
+            width: 2
+          }
+        };
+        //var point = new Point([-77.036744, 38.897731]);
+        var pointGraphic = new Graphic({
+            geometry: graphicGeometryHome,
+            symbol: markerSymbol
+        });
+        view.graphics.add(pointGraphic);*/
+
+        /*
+        var circle = new Circle([,], {
+            radius: bufferDistance,
+            //radiusUnit: isMiles ? esri.Units.MILES : esri.Units.km,
+            geodesic: true
+        });
+
+        var circleSymbol = {
+            type: "simplefillsymbol", // autocasts as new SimpleMarkerSymbol()
+            color: [226, 119, 40],
+            style: 'STYLE_CIRCLE',
+            outline: {
+                // autocasts as new SimpleLineSymbol()
+                color: [255, 255, 255],
+                width: 2
+            }
+        };
+
+        var circleGraphic = new Graphic(circle, circleSymbol);
+        view.graphics.add(circleGraphic);
+        */
+
+
+        var sourceBuffers = geometryEngine.geodesicBuffer(
+            graphicGeometryHome,
+            homeBufferDist,
+            "meters",
+            true
+        );
+
+        var bufferGraphic = new Graphic({
+            geometry: sourceBuffers, // TODO to be filled
+            symbol: {
+              type: "simple-fill", // autocasts as new SimpleFillSymbol()
+              outline: {
+                width: 1.5,
+                color: [255, 128, 0, 0.5]
+              },
+              style: "none"
+            }
+        });
+        //view.graphics.removeAll();
+        view.graphics.add(bufferGraphic);
+
+        var featureQuery = featureLayer.createQuery();
+        featureQuery.geometry = bufferGraphic.geometry;
+        featureLayer.queryFeatures(featureQuery).then(selectInBuffer);
+    }
+
+    function selectInBuffer(response){
+      var feature;
+      var features = response.features;
+      var inBuffer = [];
+      console.log(features);
+      //filter out features that are not actually in buffer, since we got all points in the buffer's bounding box
+      for (var i = 0; i < features.length; i++) {
+        feature = features[i];
+        if(circle.contains(feature.geometry)){
+          inBuffer.push(feature.attributes[featureLayer.objectIdField]);
+        }
+      }
+      console.log("In Buffer" , inBuffer.length);
+      var query = new Query();
+      query.objectIds = inBuffer;
+      //use a fast objectIds selection query (should not need to go to the server)
+      featureLayer.selectFeatures(query, FeatureLayer.SELECTION_NEW, function(results){
+        results.forEach(function (result) {
+            console.log(result.attributes.Car_Make);
+        })
+      });
     }
 });
